e1e31e3b841dcb69116cc38959f5ab0251bd7734
ayra <127664622+dumbcoffee@users.noreply.github.com>
2023-08-23 21:30:28 +0800

    Add files via upload

---
+++ TriangularWave.m
@@ -0,0 +1,19 @@
+%Triangular wave
+
+%Creates a plot of triangular wave
+%t is input row vector in seconds
+%A is scalar amplitude
+%f is scalar frequency in hertz
+
+function TriangularWave(t, A, f)
+% A = 8/(pi*pi);
+% t = 0:0.01:1;
+% f = 1;
+N = 100000;
+fx = 0;
+
+for n = 1:2:N
+    fx = fx + A * (((-1)^((n-1)/2))/(n*n))* sin(2*pi*n*f*t);
+end
+
+plot(x,fx);
