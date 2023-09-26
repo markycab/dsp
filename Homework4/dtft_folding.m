55de9c00ccbf7181afed1aeec4a1ed78b0272eb7
viejye <95478631+viejye@users.noreply.github.com>
2023-09-26 23:21:05 +0800

    Create dtft_folding.m

---
+++ Homework4/dtft_folding.m
@@ -0,0 +1,18 @@
+%TURNO
+n = -5:10;
+x = rand(1,length(n)); % Generating a random signal
+
+k = -100:100; 
+w = (pi/100)*k; % Frequencies between -pi and +pi
+
+% Compute DTFT of x
+X = x * (exp(-1j*pi/100)).^(n'*k);
+
+% Applying folding property
+y = fliplr(x); 
+m = -fliplr(n); % Signal folding
+Y = y * (exp(-1j*pi/100)).^(m'*k); % DTFT of y
+
+% Verification
+Y_check = fliplr(X); % X(-w)
+error = max(abs(Y - Y_check))% Difference
