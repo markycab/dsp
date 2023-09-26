6a5322db5955dd660172461eb08ebe0af9caed98
viejye <95478631+viejye@users.noreply.github.com>
2023-09-26 22:58:17 +0800

    Add files via upload

---
+++ Homework4/dftf_timeshifting.m
@@ -0,0 +1,14 @@
+%TURNO, VIANELYN JAN 
+x = rand(1,11); n = 0:10;
+k = 0:500; w = (pi/500)*k;
+
+% Compute DTFT of x
+X=x* (exp(-j*pi/500)).^(n'*k);
+
+% Signal shifted by two samples
+y = x; m = n+2;
+Y=y* (exp(-j*pi/500)).^(m'*k); % DTFT of y
+
+% Verification
+Y_check = (exp(-j*2).^w).*X; % Multiplication by exp(-j2w)
+error = max(abs(Y-Y_check))
