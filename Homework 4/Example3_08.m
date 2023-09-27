58d172471141e0ff6612bbbefa35a467cb516db0
Chezter Vargas <142224617+Cheshire-V@users.noreply.github.com>
2023-09-26 19:26:29 -0700

    Add files via upload

---
+++ Homework 4/Example3_08.m
@@ -0,0 +1,18 @@
+% Generate a random signal with frequency indices
+x = rand(1,11); n = 0:10;
+k = 0:500; w = (pi/500)*k;
+
+% Compute DTFT of x
+X = x * (exp(-1i * pi / 500)).^(n'*k);
+
+% Signal shifted by two samples
+y = x; m = n+2;
+Y = y * (exp(-1i * pi / 500)).^(m'*k); % DTFT of y
+
+% Verification
+Y_check = (exp(-1i * 2).^w).*X; % Multiplication by exp(-j2w)
+error = max(abs(Y-Y_check));
+
+disp(['error: ', num2str(error)]);
+
+%Chezter P. Vargas
