70eae58253831392603227b18f5f67baef9d2928
Quickfixx <142247936+Quickfixx@users.noreply.github.com>
2023-09-28 13:11:02 +0800

    Add files via upload

---
+++ Homework 4_7 .m
@@ -0,0 +1,29 @@
+% John Flynn Bontia
+% Homework 4
+x1 = rand(1,11);
+x2 = rand(1,11);
+
+% time index
+n = 0:10; 
+
+% Scaling factors
+alpha = 2;
+beta = 3;
+
+% Frequency index
+k = 0:500;
+w = (pi/500)*k; 
+
+X1 = x1 * (exp(-1j*pi/500)).^(n'*k); 
+X2 = x2 * (exp(-1j*pi/500)).^(n'*k); 
+
+% Linear combination of both
+x = alpha*x1 + beta*x2; 
+% Computation
+X = x * (exp(-1j*pi/500)).^(n'*k);  
+
+% Verifying
+X_check = alpha*X1 + beta*X2; 
+
+% Calculation
+error = max(abs(X-X_check));
